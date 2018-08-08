f7827de0d958448cb507d1b5b5cc9e8b4f60000b
root <root@vps484443.ovh.net>
2018-08-08 17:57:50 +0200

    Removing useless file

--- src/Components/Taskbar.js
+++ src/Components/Taskbar.js
@@ -1,15 +1,27 @@
 import React, {Component} from 'react';
+import Clock from "./Clock";
 import styled from 'styled-components';
 
-const Navbar = styled.header`
-	background-color: black;
+const Bar = styled.header`
+	background-color: rebeccapurple;
 	height: 50px;
+	color: white;
+	line-height: 50px;
 `;
+
+const ClockWrapper = styled.div`
+	position: absolute;
+	right: 10px;
+`
 
 class TaskBar extends Component {
   render() {
     return (
-		<Navbar />
+		<Bar>
+			<ClockWrapper>
+				<Clock />
+			</ClockWrapper>
+		</Bar>
     );
   }
 }
