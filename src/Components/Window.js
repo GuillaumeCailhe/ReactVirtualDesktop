fbdea830990b7490767e971f45b8e98e31e5df3d
Guillaume Cailhe <root@vps484443.ovh.net>
2018-08-15 16:38:03 +0200

    Removed debug message

--- src/Components/Window.js
+++ src/Components/Window.js
@@ -201,7 +201,6 @@ class Window extends Component {
 		      	</WindowHeader>
 
 		      	<WindowBody>
-		      		{this.props.zIndex}
 		      		{this.props.children}
 		      	</WindowBody>  
     		</ResizableWindow>
